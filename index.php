e02a6b029f0e3c981c97606b9dfddf6632ac9228
Thasin <thasinmahmud.bd@gmail.com>
2021-03-24 18:03:30 +0600

    Index page basic structure created & design added

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Goals and resolutions</title>
+
+    <link rel="stylesheet" href="Style_css/index_style.css">
+
+</head>
+<body>
+    <div class="container">
+
+        <div class="head_line">
+            <p>Goals & Resolutions</p>
+        </div>
+
+        <div class="section">
+
+            <div class="links">
+
+                <div class="view">
+                    <a href="#">
+                        <img src="Media/icons/Eye_100px.png" width="25px" alt="view">
+                    </a>
+                </div>
+
+                <div class="create">
+                   <a href="#">
+                       <img src="Media/icons/Create_104px.png" width="25px" alt="create">
+                   </a>
+                </div>
+
+                <div class="edit_delete">
+                    <a href="#">
+                        <img src="Media/icons/Edit_100px.png" width="25px" alt="edit or delete">
+                    </a>
+                </div>
+
+            </div>
+
+            <div class="posts">
+
+                <div class="date">
+                    <p>Date</p>
+                </div>
+
+                <div class="goals">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad vel officia enim nemo quisquam quidem aut, natus esse earum iste.</p>
+                </div>
+
+                <div class="goals_status">
+                    <form action="index.html" method="post">
+                        <button class="complete_btn" type="submit" name="completed">Completed</button>
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+</body>
+</html>
